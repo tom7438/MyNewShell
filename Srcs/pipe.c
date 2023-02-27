648d5d132b9a27d58b00e777810773c3fcb19748
theo koehler <theokoehler.38160@gmail.com>
2023-02-27 13:45:03 +0100

    début v3

--- Srcs/pipe.c
+++ Srcs/pipe.c
@@ -63,6 +63,73 @@ int Mypipe(struct cmdline * command) {
     return 0;
 }
 
+int Multipipe(struct cmdline * command, int i) {
+    /* plusieur un pipe */
+    int num_pipes = i-1;
+    int pipes[num_pipes][2];
+
+    // Création des pipes
+    for (int i = 0; i < num_pipes; i++) {
+        if (pipe(pipes[i]) < 0) {
+            perror("Erreur lors de la création du pipe");
+            return 1;
+        }
+    }
+
+    pid_t pid[2][i];
+    int status;
+
+    if((pid[0] = Fork()) == 0) {
+        /* Fils */
+        if(command->in != NULL){
+            int fd_in = Open(command->in, O_RDONLY, 0);
+            Dup2(fd_in, STDIN_FILENO);
+            Close(fd_in);
+        }
+        Dup2(fd[1], STDOUT_FILENO);
+        Close(fd[0]);
+        Close(fd[1]);
+        if(isCommandeInterne(command->seq[0][0])) {
+            executeCommandeInterne(command->seq[0][0], command->seq[0]);
+            exit(0);
+        } else if(execvp(command->seq[0][0], command->seq[0]) < 0){
+            printf("Commande externe non reconnue: %s\n", command->seq[0][0]);
+            exit(0);
+        }
+    }
+    else {
+        /* Pere */
+        if((pid[1] = Fork()) == 0) {
+            /* Fils 2 */
+            if(command->out != NULL){
+                int fd_out = Open(command->out, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+                Dup2(fd_out, STDOUT_FILENO);
+                Close(fd_out);
+            }
+            Dup2(fd[0], STDIN_FILENO);
+            Close(fd[0]);
+            Close(fd[1]);
+            if(isCommandeInterne(command->seq[1][0])) {
+                executeCommandeInterne(command->seq[1][0], command->seq[1]);
+                exit(0);
+            } else if(execvp(command->seq[1][0], command->seq[1]) < 0){
+                printf("Commande externe non reconnue: %s\n", command->seq[1][0]);
+                exit(0);
+            }
+        }
+        else {
+            /* Pere */
+        // Fermeture des pipes
+        for (int i = 0; i < num_pipes; i++) {
+            close(pipes[i][0]);
+            close(pipes[i][1]);}
+            Waitpid(pid[0], &status, 0);
+            Waitpid(pid[1], &status, 0);
+        }
+    }
+    return 0;
+}
+
 int commande(struct cmdline * command) {
     /* Commande interne */
     if(command->seq[0] == NULL) {
@@ -108,6 +175,7 @@ int commande(struct cmdline * command) {
         }
         Waitpid(pid, &status, 0);
     }
+    
     /* Reinitialiser stdin et stdout */
     if(oldin!=-1)
         Dup2(oldin, STDIN_FILENO);
--- Srcs/shell3.c
+++ Srcs/shell3.c
@@ -1,4 +1,61 @@
 //
-// Created by tom on 23/02/23.
+// Created by théo on 23/02/23.
 //
 
+int main() {
+    int couleur = 31;
+    /* Ignore les signaux Ctrl-C et Ctrl-Z */
+    Signal(SIGINT, sigint_handler);
+    Signal(SIGTSTP, SIG_IGN);
+	while (1) {
+		struct cmdline *command;
+
+        /* Affichage du prompt */
+        char *rep=(char *)malloc(sizeof(char)*100);
+        printf("\033[%dm", couleur);
+        printf("\033[4mMyShell2\033[00m");
+        printf(":");
+        if(!strcmp(getenv("HOME"), getcwd(rep, 100))){
+            printf("\033[34m~\033[00m");
+        } else{
+            printf("\033[34m%s\033[00m",getcwd(rep, 100));
+        }
+        printf(" # ");
+        if((couleur=(couleur+1-31)%18+31)==34){couleur++;};
+
+		command = readcmd();
+
+		/* If input stream closed, normal termination */
+		if (!command) {
+			printf("exit\n");
+			exit(0);
+		}
+
+        /* Si Ctrl-C est pressé */
+        if(sigint_flag){
+            sigint_flag = 0;
+            continue;
+        }
+
+		if (command->err) {
+			/* Syntax error, read another command */
+			printf("error: %s\n", command->err);
+			continue;
+		}
+
+        int i = 0;
+        while(command->seq[i] != NULL){
+            i++;
+        }
+
+        if(i > 1){
+            /* plusieur pipe */
+            Mypipe(command,i);
+        } else if (i == 1){
+            /* 1 commande unique */
+            commande(command);
+        } else {
+            printf("error: too many commands, not yet implemented\n");
+        }
+    }
+}
